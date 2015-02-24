43e2f64e6900624d1f2af518f333f5d4d30d7d1d
HELLO CONAN <nan@guest-wireless-207-151-035-014.usc.edu>
2015-02-23 22:55:39 -0800

    drawer2

--- app/main/scripts/DrawerController.js
+++ app/main/scripts/DrawerController.js
@@ -3,5 +3,30 @@ angular
   .controller('DrawerController', function($scope, supersonic) {
 
     $scope.navbarTitle = "Drawer";
-
+    //$scope.courses = "";
+     $scope.toggleCourse = function(course) {
+		  if (course.expanded === true) {
+			  course.expanded = false;
+		  } else {
+			  course.expanded = true;
+		  }
+	  };
+	  
+	  $scope.isGroupShown = function(course) {
+		  if (course.expanded === true) {
+			  return true;
+		  } else {
+			  return false;
+		  }
+	  };
+	  
+	 
+	  // filter functions - set isEnabledByDay, isEnabledByTime, isEnabledByCode, isEnabledByUnits
+	  $scope.isCourseRegistered = function(course) {
+		  return course.isRegistered;
+	  };
+	  
+	  $scope.isSectionEnabled = function (section) {
+		  return section.isEnabledByDay && section.isEnabledByTime && section.isEnabledByCode && section.isEnabledByUnits;
+	  };
   });
