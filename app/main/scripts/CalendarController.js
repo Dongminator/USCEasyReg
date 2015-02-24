3d1dfd5d8b4c19828e3fbde595eb451bc90de813
Donglin Pu <Dongminator@guest-wireless-207-151-058-015.usc.edu>
2015-02-23 16:10:29 -0800

    donglin changes

--- app/main/scripts/CalendarController.js
+++ app/main/scripts/CalendarController.js
@@ -5,3 +5,49 @@ angular
     $scope.navbarTitle = "Calendar";
 
   });
+
+
+$(document).ready(function() {
+
+    // page is now ready, initialize the calendar...
+
+    $('#calendar').fullCalendar({
+        // put your options and callbacks here    
+        header:{
+            left:'',
+            center:'',
+            right:''            
+        },
+        height: 1175,
+        slotDuration: '00:15:00',
+        weekends: false,
+        defaultView: 'agendaWeek',
+        handleWindowResize: false,
+        allDaySlot: false,
+        minTime: '06:00:00',
+        maxTime: '23:00:00',
+        events: [
+            {
+					title: 'Lunch',
+					start: '2015-02-23T12:00:00'
+				},
+				{
+					title: 'Meeting',
+					start: '2015-02-24T14:30:00'
+				},
+				{
+					title: 'Happy Hour',
+					start: '2015-02-24T17:30:00'
+				},
+				{
+					title: 'Dinner',
+					start: '2015-02-25T20:00:00'
+				},
+				{
+					title: 'Birthday Party',
+					start: '2015-02-26T07:00:00'
+				}
+        ]
+    });
+
+});
