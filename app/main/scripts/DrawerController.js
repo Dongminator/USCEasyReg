61162d382cca9940b3a69892367c89564ed1e635
Donglin Pu <Dongminator@guest-wireless-207-151-058-015.usc.edu>
2015-02-24 00:19:50 -0800

    changed color

--- app/main/scripts/DrawerController.js
+++ app/main/scripts/DrawerController.js
@@ -2,12 +2,9 @@ angular
   .module('main')
   .controller('DrawerController', function($scope, supersonic) {
 
-    $scope.navbarTitle = "Drawer";
-
-    
-    
-    $scope.getJson = function () {
-    	var courses = window.localStorage.getItem('EasyReg.interestedCourses');
-        supersonic.logger.debug(courses);
-    };
+	  
+	  $scope.getJson = function () {
+		  var courses = window.localStorage.getItem('EasyReg.interestedCourses');
+		  supersonic.logger.debug("Courses: " + courses);
+	  };
   });
